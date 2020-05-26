0f1a0949f1b7b3a3cc64435b3974b510aa70c5ac
lym1196786647 <1196786647@qq.com>
2020-05-26 16:14:51 +0800

    新建页面： 我的申请，个人中心 修改页面： 通知页面

--- untitled10/untitled10/staticView.py
+++ untitled10/untitled10/staticView.py
@@ -8,3 +8,9 @@ def notice(request):
     return render(request,"./notice.html");
 def myApplication(request):
     return render(request,"./my_application.html");
+def roomclass(request):
+    return render(request,"./room_select.html");
+def manage(request):
+    return render(request,"./person_management.html");
+def adddel(request):
+    return render(request,"./person_adddel.html");
