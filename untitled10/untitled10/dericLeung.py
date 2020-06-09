598128b3b3c698ab8a7c3ec3fe678d6a739adb65
congcong0419 <yicong687399@163.com>
2020-06-09 12:04:59 +0800

    自动通知做完

--- untitled10/untitled10/dericLeung.py
+++ untitled10/untitled10/dericLeung.py
@@ -16,12 +16,6 @@ def checksession(request):
     else:
         return False
 
-
-
-
-
-
-
 def login_check(request):
     request.session.clear_expired()
     username = ""
@@ -61,7 +55,6 @@ def getavailableres(request):
 
     return render(request,'./resource_borrowable.html',{'resource':result,'username':username,'type':type,'size':size})
 
-
 def getdate(request):
     with connection.cursor() as cursor:
         sql = 'SELECT startdate FROM userModel_record WHERE date('')'
@@ -69,7 +62,6 @@ def getdate(request):
         result = cursor.fetchall()
         res = ''
         return HttpResponse(result)
-
 
 def searchres(request):
     username = checksession(request)
@@ -94,7 +86,6 @@ def searchres(request):
             result,size=getAvaRes(page=page,type=type,time=time,place=place)
             return render(request, './resource_borrowable.html', {'resource': result, 'username': username, 'type': type,'size':size})
 
-
 def isDate(str):
     try:
         datetime.strptime(str,"%Y-%m-%d")
@@ -106,10 +97,10 @@ def getAvaRes(type,page,time='now',place=''):#两个都是string
     with connection.cursor() as cursor:
         sql=''
         if(place==''):
-            sql = 'SELECT name FROM userModel_record a, userModel_resource b WHERE a.resource_id = b.id and b.type = "'+ type +'" and b.isavailable = "True"' \
+            sql = 'SELECT name FROM userModel_resource b WHERE b.type = "'+ type +'" and b.isavailable = "True"' \
                   'and b.id not in (SELECT resource_id FROM userModel_record where startdate <= date("'+time+'") and enddate>=date("'+time+'") and state = "预约成功")'
         else:
-            sql= 'SELECT name FROM userModel_record a, userModel_resource b WHERE a.resource_id = b.id and b.type = "'+ type +'" and b.isavailable = "True"' \
+            sql= 'SELECT name FROM userModel_resource b WHERE b.type = "'+ type +'" and b.isavailable = "True"' \
                   'and b.id not in (SELECT resource_id FROM userModel_record where startdate <= date("'+time+'") and enddate>=date("'+time+'") and state = "预约成功") ' \
                 'and b.location="'+place+'"'
         cursor.execute(sql)
@@ -118,6 +109,7 @@ def getAvaRes(type,page,time='now',place=''):#两个都是string
         for i in range(0,len(result)):
             tempdic={'id':i+1,'name':result[i][0]}
             res.append(tempdic)
+        print(sql)
         size = (len(res)-1)//7+1
         resdic={}
         for i in range(0,size):
@@ -145,41 +137,41 @@ def resourcemanage(request):
     result,size = getmyres(page=str(page),id=id)
     return render(request,'./resource_management.html',{'isroot':isroot,'username':username,'size':size,'resource':result,'page':page})
 
-# def othernotice(request):
-#     username = checksession(request)
-#     if (username == False):
-#         return redirect('/login')
-#     page=request.GET.get('page','1')
-#     id=str(request.session.get('userid',''))
-#
-#     result,size = getmyres(page=str(page),id=id)
-#     return render(request,'./else_notice.html',{'username':username,'size':size,'resource':result,'page':page})
-#
-#
-# def my_res(request):
-#     username = checksession(request)
-#     if (username == False):
-#         return redirect('/login')
-#     page = request.GET.get('page', '1')
-#     id = str(request.session.get('userid', ''))
-#
-#     result, size = getmyres(page=str(page), id=id)
-#     return render(request, './else_notice.html', {'username': username, 'size': size, 'resource': result, 'page': page})
+def othernotice(request):
+    username = checksession(request)
+    if (username == False):
+        return redirect('/login')
+    page=request.GET.get('page','1')
+    id=str(request.session.get('userid',''))
+
+    result,size = getmyres(page=str(page),id=id)
+    return render(request,'./else_notice.html',{'username':username,'size':size,'resource':result,'page':page})
+
+
+def my_res(request):
+    username = checksession(request)
+    if (username == False):
+        return redirect('/login')
+    page = request.GET.get('page', '1')
+    id = str(request.session.get('userid', ''))
+
+    result, size = getmyres(page=str(page), id=id)
+    return render(request, './else_notice.html', {'username': username, 'size': size, 'resource': result, 'page': page})
 
 def getmyres(page='1',id=None):
     if(id=='1'):
-        sql = 'select name,isavailable from userModel_resource a ,userModel_resourcebelonging b ' \
+        sql = 'select name,isavailable,a.id from userModel_resource a ,userModel_resourcebelonging b ' \
               'where a.id=b.resource_id'
 
     else:
-        sql = 'select name,isavailable from userModel_resource a ,userModel_resourcebelonging b ' \
+        sql = 'select name,isavailable,a.id from userModel_resource a ,userModel_resourcebelonging b ' \
               'where a.id=b.resource_id and b.owner_id='+id
     with connection.cursor() as cursor:
         cursor.execute(sql)
         result = cursor.fetchall()
         res=[]
         for index in range(0, len(result)):
-            tempdic={'id':index+1, 'name':result[index][0]}
+            tempdic={'id':index+1, 'name':result[index][0],'resid':result[index][2]}
             if(result[index][1]=='True'):
                 tempdic['state']='可借用'
             else:
@@ -234,6 +226,11 @@ def addrecord(request):
     if(resname==''):
         return HttpResponse('ERROR')
     else:
+
+
+
+
+        #申请开始
         with connection.cursor() as cursor:
             sql = 'select id from userModel_resource where name = "'+ resname +'"'
             cursor.execute(sql)
@@ -260,30 +257,73 @@ def delresource(request):
         return redirect('/login')
     if (request.method == 'POST'):
         changeitems = request.POST.getlist('shift', [])
+        print(changeitems)
         thispage = request.GET.get('page', '')
         sqlread = 'delete from userModel_resource'
-        sql = 'delete from userModel_resourcebelonging'
+        sql2 = 'delete from userModel_resourcebelonging'
         sql1 = 'delete from userModel_record'
         if (len(changeitems) != 0):
             sqlread += ' where id in ('
-            sql +=' where resource_id in('
+            sql2 +=' where resource_id in('
             sql1 += ' where resource_id in('
-
+            group = '('
             for id in changeitems:
-                sql += ' ' + id + ','
+                sql2 += ' ' + id + ','
                 sqlread += ' ' + id + ','
                 sql1+= ' ' + id + ','
+                group += ' ' + id + ','
             sqlread = sqlread[:-1] + ')'
-            sql = sql[:-1]+')'
+            sql2 = sql2[:-1]+')'
             sql1 = sql1[:-1] + ')'
+            group = group[:-1]+')'
+
+            resnamesql = 'select id,name from userModel_resource where id in '+group
+            with connection.cursor() as cursor:
+                cursor.execute(resnamesql)
+                rs=cursor.fetchall()
+
+                if(len(rs)!=0):
+                    for item in rs:
+                        resname = item[1]
+                        id = item[0]
+                        text = resname + ':资源已被删除，您的申请记录已取消'
+                        restxt = resname + ':资源已取消，您不再拥有这个资源'
+                        sql = 'select user_id from userModel_record where resource_id = "' + str(id) + '"'
+                        cursor.execute(sql)
+                        res = cursor.fetchall()
+                        sql = 'insert into userModel_othernotice(text,isread,user_id) values("{}","False","{}")'
+                        if (len(res) != 0):
+                            for item in res:
+                                id = item[0]  # 获取影响的用户id
+                                sqltemp = sql.format(text, str(id))
+                                cursor.execute(sqltemp)
+                        else:
+                            print("没有人租用这个资源")
+
+                        sql = 'select owner_id from userModel_resourcebelonging where resource_id = "{}"'
+                        sql = sql.format(str(id))
+                        cursor.execute(sql)
+                        res = cursor.fetchall()
+                        sql = 'insert into userModel_othernotice(text,isread,user_id) values("{}","False","{}")'
+                        if (len(res) != 0):
+                            for item in res:
+                                id = item[0]
+                                sqltemp = sql.format(restxt, id)
+                        else:
+                            print('error ! 没有人拥有这个资源')
+
+
+            #执行删除
             with connection.cursor() as cursor:
                 cursor.execute(sql1)
-                cursor.execute(sql)
+                cursor.execute(sql2)
                 cursor.close()
             with connection.cursor() as cursor:
                 cursor.execute(sqlread)
                 return redirect('/myresource/?page=1')
             return HttpResponse(sqlread)
+        else:
+            return redirect('/myresource/?page='+thispage)
 
 
 def addresource(request):
